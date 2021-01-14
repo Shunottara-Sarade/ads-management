3abb3fa4b5ae89b40add2baaec4c33436fd6be1d
Shunottara-Sarade <74823403+Shunottara-Sarade@users.noreply.github.com>
2021-01-14 23:45:38 +0530

    dakshata commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,10 +3,11 @@ import React from 'react';
 import Cards from '../components/Cards';
 import HeroSection from '../components/HeroSection';
 import Footer from '../components/Footer';
-
+import Navbar from '../components/Navbar';
 function Home() {
   return (
     <>
+    <Navbar/>
       <HeroSection />
       <Cards />
       <Footer />
