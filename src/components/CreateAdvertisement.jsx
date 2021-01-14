c0a27e4b630d3333a89cf5031d31ab08fedbd1f4
Shunottara-Sarade <74823403+Shunottara-Sarade@users.noreply.github.com>
2021-01-14 23:26:00 +0530

    sapna commit

---
+++ src/components/CreateAdvertisement.jsx
@@ -0,0 +1,204 @@
+import React, { Component } from 'react';
+import AdvertisementService from './services/AdvertisementService';
+// import Cookies from 'universal-cookie';
+
+class CreateAdvertisementComponent extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            advType: '',
+            advLocation: '',
+            createdBy: '',
+            startDate: '',
+            endDate: '',
+            advImage: ''
+        }
+        this.changeAdvTypeHandler = this.changeAdvTypeHandler.bind(this);
+        this.changeAdvLocationHandler = this.changeAdvLocationHandler.bind(this);
+        this.changeCreatedByHandler = this.changeCreatedByHandler.bind(this);
+        this.changeStartDateHandler = this.changeStartDateHandler.bind(this);
+        this.changeEndDateHandler = this.changeEndDateHandler.bind(this);
+        this.changeAdvImageHandler = this.changeAdvImageHandler.bind(this);
+        this.saveAdvertisement = this.saveAdvertisement.bind(this);
+    }
+
+    saveAdvertisement = (e) => {
+        e.preventDefault();
+
+        let advertisement = {
+            id: null, advType: this.state.advType, advLocation: this.state.advLocation,
+            createdBy: this.state.createdBy, startDate: this.state.startDate, endDate: this.state.endDate
+            
+        };
+        console.log(JSON.stringify(advertisement));
+        AdvertisementService.createAdvertisement(advertisement).then(res => {
+            //alert("Advertisement created");
+            if(this.state.createdBy==="Own")
+            {
+                //document.getElementById("uploadImg").disabled=false;
+                if(window.confirm("Proceed to upload image?"))
+                {
+                    this.props.history.push('/upload-image');
+                }
+                else
+                {
+                    this.props.history.push('/add-advertisement');
+                }
+            }
+            else if(this.state.createdBy==="Ops Team")
+            {
+               alert("Advertisement created")
+                this.props.history.push('/advertisement');
+            }
+            
+        })
+    }
+
+
+    cancel() {
+        this.props.history.push('/advertisement');
+    }
+
+    uploadImage(){
+        this.props.history.push('/upload-image');
+    }
+
+    changeAdvTypeHandler = (event) => {
+        this.setState({ advType: event.target.value });
+    }
+
+    changeAdvLocationHandler = (event) => {
+        this.setState({ advLocation: event.target.value });
+    }
+
+    changeCreatedByHandler = (event) => {
+        this.setState({ createdBy: event.target.value });
+    }
+
+    changeStartDateHandler = (event) => {
+        this.setState({ startDate: event.target.value });
+    }
+
+    changeEndDateHandler = (event) => {
+        this.setState({ endDate: event.target.value });
+    }
+
+    
+
+    changeAdvImageHandler = (event) => {
+        this.setState({ advImage: event.target.value });
+    }
+
+    // changeOwnHandler = () => {
+    //     document.getElementById("uploadImg").disabled = false;
+    // }
+
+    // changeOpsTeamHandler = () => {
+    //     document.getElementById("uploadImg").disabled = true;
+    // }
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <p></p>
+                </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="card col-md-6 offset-md-3 offset-md-3">
+                            <h3 className="text-center">Add Advertisement</h3>
+                            <div className="card-body">
+                                <fieldset>
+                                    <form>
+                                        <div className="form-group">
+                                            <label>Select Advertisement Type</label>
+                                            <select name="advType"
+                                                className="form-control" value={this.state.advType}
+                                                onChange={this.changeAdvTypeHandler}>
+                                                <option value="Choose">Choose Advertisement Type</option>
+                                                <option value="Electronics">Electronics</option>
+                                                <option value="Spices">Spices</option>
+                                                <option value="Clothing">Clothing</option>
+                                                <option value="Commercial">Commercial</option>
+                                            </select>
+                                        </div>
+
+                                        <div className="form-group">
+                                            <label>Enter Location</label>
+                                            <input placeholder="Advertisement Location"
+                                                name="advLocation"
+                                                className="form-control"
+                                                value={this.state.advLocation}
+                                                onChange={this.changeAdvLocationHandler} />
+                                        </div>
+
+                                        <div className="form-group">
+                                            <label>Select Created By</label><br></br>
+                                            <input type="radio" name="createdBy" id='createdBy'
+                                                value="Own" style={{ marginRight: '15px' }}
+                                                onChange={this.changeCreatedByHandler}
+                                                onClick={this.changeOwnHandler} />
+                                            <label> Own</label><br></br>
+                                            <input type="radio" name="createdBy"
+                                                value="Ops Team" style={{ marginRight: '15px' }}
+                                                onChange={this.changeCreatedByHandler}
+                                                onClick={this.changeOpsTeamHandler} />
+                                            <label> Operational Team</label>
+                                        </div>
+
+                                        <div className="form-group">
+                                            <label>Select Start Date</label>
+                                            <input type="date" name="startDate"
+                                                className="form-control"
+                                                value={this.state.startDate}
+                                                onChange={this.changeStartDateHandler} />
+                                        </div>
+
+                                        <div className="form-group">
+                                            <label>Select End Date</label>
+                                            <input type="date" name="endDate"
+                                                className="form-control" value={this.state.endDate}
+                                                onChange={this.changeEndDateHandler} />
+                                        </div>
+
+                                        {/* <div className="form-group">
+                                        <label>Enter Staff Id</label>
+                                        <input placeholder="Staff Id" name="staffId"
+                                            className="form-control" value={this.state.staffId}
+                                            onChange={this.changeStaffIdHandler} />
+                                    </div>
+                                    <div className="form-group">
+                                        <label>Enter Customer Id</label>
+                                        <input placeholder="Customer Id" name="custId"
+                                            className="form-control" value={this.state.custId}
+                                            onChange={this.changeCustIdHandler} />
+                                    </div> */}
+
+                                        <button className="btn btn-success" onClick={this.saveAdvertisement}>Save</button>
+                                        <button className="btn btn-danger" onClick={this.cancel.bind(this)} style={{ marginLeft: "10px" }}>Cancel</button>
+                                       
+                                    </form>
+
+                                </fieldset>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <div>
+                    {/* <form>
+                    <p> <button className="btn btn-primary" id="uploadImg" onClick={this.uploadImage.bind(this)} style={{ marginLeft: "10px" }} disabled>Upload Image</button></p>
+                    </form> */}
+                </div>
+                <div>
+                    <p id="cookie"></p>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+
+export default CreateAdvertisementComponent;
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+function Footer() {
+  return (
+    <div className='footer-container'>
+      {/* <section className='footer-subscription'> */}
+        <p className='footer-subscription-heading'>
+          All right reserved to add management teams
+        </p>
+    </div>
+  );
+}
+
+export default Footer;
