31ddeeb54fe9f98f659f51560e8d70dd84678618
Shunottara-Sarade <74823403+Shunottara-Sarade@users.noreply.github.com>
2021-01-14 23:44:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { BrowserRouter as Router, Route,Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './components/Login';
 import DashBoard from './components/Dashboard';
 import Register from './components/Register';
 import Logout from './components/Logout';
-import Navbar from './components/Navbar';
 import './App.css';
 import Home from './pages/Home';
 import Electronic from './components/Electronic';
@@ -14,37 +13,37 @@ import Jobs from './components/Jobs';
 import ContactUs from './pages/ContactUs';
 import LoginInitial from './components/LoginInitial';
 import StaffLogin from './components/StaffLogin';
+import AdminDashboard from './components/AdminDashboard';
+import CreateAdvertisementComponent from './components/CreateAdvertisement';
+import UploadImage from './components/UploadImage';
 
 class App extends React.Component {
-render() {
-return (
-<Router>
-<Navbar />
-<Switch>
-    <Route path='/' exact component={Home} />
-          <Route path='/electronic' component={Electronic} />
-          <Route path='/restaurant' component={Restaurant} />
-          <Route path='/hotel' component={Hotel} />
-          <Route path='/jobs' component={Jobs} />
-          <Route path='/home' component={Home} />
-          <Route path='/contactUs' component={ContactUs} />
-<Route path="/Login" component={Login} />
-<Route path="/Dashboard" component={DashBoard}/>
-<Route path="/Register" component={Register}/>
-<Route path="/Logout" component={Logout}/>
-<Route path='/sign-up' component={LoginInitial} />
-<Route path="/staff-login" component={StaffLogin}/>
-</Switch>
-</Router>
-);
+    render() {
+        return (
+            <Router>
+                
+                <Switch>
+                    <Route path='/' exact component={Home} />
+                    <Route path='/electronic' component={Electronic} />
+                    <Route path='/restaurant' component={Restaurant} />
+                    <Route path='/hotel' component={Hotel} />
+                    <Route path='/jobs' component={Jobs} />
+                    <Route path='/home' component={Home} />
+                    <Route path='/contactUs' component={ContactUs} />
+                    <Route path="/Login" component={Login} />
+                    <Route path="/Dashboard" component={DashBoard} />
+                    <Route path="/Register" component={Register} />
+                    <Route path="/Logout" component={Logout} />
+                    <Route path='/sign-up' component={LoginInitial} />
+                    <Route path="/staff-login" component={StaffLogin} />
+                    <Route path="/admin-dashboard" component={AdminDashboard}/>
+                    <Route path= "/add-advertisement" component={CreateAdvertisementComponent}/>
+                    <Route path="/upload-image" component={UploadImage}/>
+                </Switch>
+            </Router>
+        );
 
-}
+    }
 
 }
 export default App;
-
-
-
-
-
-
