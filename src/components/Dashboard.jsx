2288297b5261ae3d503e241855cdf7e140bc5664
Shunottara-Sarade <74823403+Shunottara-Sarade@users.noreply.github.com>
2021-01-15 08:54:36 +0530

    shunottara commit

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Navbar,NavbarToggler,Collapse,Nav,NavItem,Button } from "reactstrap";
 const App = () => {
     let data = localStorage.getItem('customer');
     data = JSON.parse(data);
     return (
         <div>
-            <p>{data.email}</p>
+            <Navbar color="faded"  light expand="md">
+                <NavbarToggler  />
+                <Collapse  navbar>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <Link to="/Logout"><Button>Logout</Button></Link>
+                        </NavItem>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+            <p>Logged as : {data.email}</p>
+            
+           <Link to="/add-advertisement"><button type="button" class="btn btn-primary">Apply for Advertisement</button></Link> 
         </div>
+        
     );
 };
 export default App;
