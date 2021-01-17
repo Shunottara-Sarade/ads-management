956b6c2f68ddea386285a518a4f7135596047e85
Shunottara-Sarade <74823403+Shunottara-Sarade@users.noreply.github.com>
2021-01-17 20:44:36 +0530

    commit from ashwini

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,49 @@
 import React from 'react';
 import './Footer.css';
+import { Link } from 'react-router-dom';
+
 function Footer() {
   return (
     <div className='footer-container'>
-      {/* <section className='footer-subscription'> */}
-        <p className='footer-subscription-heading'>
-          All right reserved to add management teams
-        </p>
+      <section className='footer-subscription'>
+
+      </section>
+      <div class='footer-links'>
+        <div className='footer-link-wrapper'>
+          <div class='footer-link-items'>
+            <Link to='/contactUs'><h2>Contact Us</h2></Link>
+
+          </div>
+
+        </div>
+        <div className='footer-link-wrapper'>
+
+        </div>
+      </div>
+      <section class='social-media'>
+        <div class='social-media-wrap'>
+          <div class='footer-logo'>
+          </div>
+          <small class='website-rights'>ALL RIGHTS RESERVED TO ADs MANAGEMENT TEAM © 2020</small>
+          <div class='social-icons'>
+
+            <a href='https://www.facebook.com' class='social-icon-link facebook' target='_blank'><i class='fab fa-facebook-f' /></a>
+
+            <a href='https://www.instagram.com' class='social-icon-link instagram' target='_blank'><i class='fab fa-instagram' /></a>
+
+            <a href='https://www.youtube.com' class='social-icon-link youtube' target='_blank'><i class='fab fa-youtube' /></a>
+
+            <a href='https://www.twitter.com' class='social-icon-link twitter' target='_blank'><i class='fab fa-twitter' /></a>
+
+            <a href='https://in.linkedin.com/' class='social-icon-link twitter' target='_blank'><i class='fab fa-linkedin' /></a>
+
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
 
 export default Footer;
+
+
