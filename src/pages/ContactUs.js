47b63bcb45a830bf3619fb4d8f75ee612615cc40
Shunottara-Sarade <74823403+Shunottara-Sarade@users.noreply.github.com>
2021-01-17 21:02:29 +0530

    commit from samidha

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,34 +1,33 @@
 import React from 'react';
-import '../css/Apps.css';
+import Footer from '../components/Footer';
+import '../css/contactUs.css';
+import Navbar from '../components/Navbar';
  
 export default function ContactUs() {
   return (
-      
-    <div className='contactUs'>
-      <h3 className='contact-tag'>Contact Us</h3>
-      <p className='para1'>Need to get in touch with our Team? We’re all ears! Here are some ways to contact us.
+    <div>
+      <Navbar/>
+     <div> 
+      <h3 className='contact-tag'>Need specific help? Get in touch with us!</h3>
+      <p className='para1'>Need to get in touch with our Team? We’re all ears! Here are <br/>some ways to contact us.
       Feel free to reach us through our email.<br></br></p>
 
  
       <div>
-          {/* <h1 className='visit-tag'>
-            Visit our Support Centre</h1>
-            <p className='para1'>Learn how to get started
-              with AdsManagementTeam,<br></br> set up your Account or just take a look at our FAQs section.
-               </p> */}
- 
+         
         <div>
-          <h1 className='email-tag'>
-            Email us</h1>
-          <p className='para1'>Our Support Team is always happy
-<br></br>to answer your questions.
+          <h1 className='email-tag'><b>
+            Email us</b></h1>
+          <p className='para1'>Our Support Team is always happy to answer your questions.<br></br>
  We will reply as soon as possible!
 </p>
+        <a class="mail" href="mailto:sapnakhatri161998@gmail.com">Email Our Team &gt;</a>
+       
         </div>
  
       </div>
-      {/* <br></br><br></br>
-      <Footer></Footer> */}
+      </div>
+     <div className='bottomFooter'><Footer/></div>
     </div>
     
  
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-//import '../../App.css';
 import Cards from '../components/Cards';
 import HeroSection from '../components/HeroSection';
 import Footer from '../components/Footer';
@@ -7,7 +6,7 @@ import Navbar from '../components/Navbar';
 function Home() {
   return (
     <>
-    <Navbar/>
+      <Navbar />
       <HeroSection />
       <Cards />
       <Footer />
